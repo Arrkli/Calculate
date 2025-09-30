5a21fa9972e2d6e50715f22296710d137b4388e1
Arrkli <nikita.erasov2008@gmail.com>
2025-09-30 14:25:59 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Calculate.go

--- Calculate.go
+++ Calculate.go
@@ -19,3 +19,33 @@ func Calculate(a, b float64, op string) (float64, error) {
 		return 0, fmt.Errorf("unsupported operation: %s", op)
 	}
 }
+
+func ParalelCalculate(a, b float64, op string, ChanelFloat64 chan float64, ChanelError chan error) {
+	var c float64
+
+	switch op {
+	case "+":
+		c = a + b
+		ChanelFloat64 <- c
+		ChanelError <- nil
+	case "-":
+		c = a - b
+		ChanelFloat64 <- c
+		ChanelError <- nil
+	case "*":
+		c = a * b
+		ChanelFloat64 <- c
+		ChanelError <- nil
+	case "/":
+		if b == 0 {
+			ChanelError <- fmt.Errorf("division by zero")
+		}
+		c = a / b
+		ChanelFloat64 <- c
+		ChanelError <- nil
+	default:
+		ChanelFloat64 <- 0
+		ChanelError <- fmt.Errorf("unsupported operation: %s", op)
+	}
+
+}
