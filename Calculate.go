40cb4a55d69409b702bb393182629bb2b0d71173
Arrkli <nikita.erasov2008@gmail.com>
2025-10-22 17:23:20 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Calculate.go

--- Calculate.go
+++ Calculate.go
@@ -65,3 +65,68 @@ func ParalelCalculate(a, b float64, op string, ChanelCalculate chan struct {
 		}{0, fmt.Errorf("unsupported operation: %s", op)}
 	}
 }
+
+func CalculateManyVar(a []float64, op string) (float64, error) {
+	var Result float64
+	var err error
+
+	for _, v := range a {
+		switch op {
+		case "+":
+			Result += v
+		case "-":
+			Result -= v
+		case "*":
+			Result *= v
+		case "/":
+			if v == 0 {
+				return 0, fmt.Errorf("division by zero")
+			}
+			Result /= v
+		default:
+			return 0, fmt.Errorf("unsupported operation: %s", op)
+		}
+	}
+
+	return Result, err
+
+}
+
+func ParalelCalculateManyVar(a []float64, op string, ChanelCalculate chan struct {
+	Value float64
+	Err   error
+}) {
+	var Result float64
+
+	for _, v := range a {
+		switch op {
+		case "+":
+			Result += v
+		case "-":
+			Result -= v
+		case "*":
+			Result *= v
+		case "/":
+			if v == 0 {
+				ChanelCalculate <- struct {
+					Value float64
+					Err   error
+				}{0, fmt.Errorf("division by zero")}
+				return
+			}
+			Result /= v
+		default:
+			ChanelCalculate <- struct {
+				Value float64
+				Err   error
+			}{0, fmt.Errorf("unsupported operation: %s", op)}
+			return
+		}
+	}
+
+	ChanelCalculate <- struct {
+		Value float64
+		Err   error
+	}{Result, nil}
+
+}
